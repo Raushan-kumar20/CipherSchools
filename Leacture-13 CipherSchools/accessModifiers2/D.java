dc5534dacc93efab953e306bd41ca84e99457cc3
Raushan-kumar20 <159602571+Raushan-kumar20@users.noreply.github.com>
2024-07-12 00:45:22 +0530

    Update D.java

--- Leacture-13 CipherSchools/accessModifiers2/D.java
+++ Leacture-13 CipherSchools/accessModifiers2/D.java
@@ -1,1 +1,11 @@
+package accessModifiers2;
+import Accessmodifiers1.A;
+public class D{
+  public static void main(String[] args){
+    A obj= new A();
+    //System.out.println(obj.x);
+  }
+  
+}
+  
 
