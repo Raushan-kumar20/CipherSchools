9f9b74a66c6aefc14c1316d8a9a0192994066ca5
Raushan-kumar20 <159602571+Raushan-kumar20@users.noreply.github.com>
2024-07-12 23:05:39 +0530

    Update A.java

--- Leacture-13 CipherSchools/Accessmodifiers1/A.java
+++ Leacture-13 CipherSchools/Accessmodifiers1/A.java
@@ -7,5 +7,8 @@ public class A{
   public static void main(String[] args){
     A obj=new A();
     System.out.println(obj.x);
+    System.out.println(obj.y);
+    System.out.println(obj.z);
+    System.out.println(obj.w);
   }
 }
