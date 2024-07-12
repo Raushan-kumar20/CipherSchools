d0af60a82c74603ae66ca1ecdbb32c7657588194
Raushan-kumar20 <159602571+Raushan-kumar20@users.noreply.github.com>
2024-07-12 23:07:02 +0530

    Update D.java

--- Leacture-13 CipherSchools/accessModifiers2/D.java
+++ Leacture-13 CipherSchools/accessModifiers2/D.java
@@ -4,9 +4,10 @@ public class D{
   public static void main(String[] args){
     A obj= new A();
     //System.out.println(obj.x);
-    System.out.println(obj.y);
-  }
-  
+    // System.out.println(obj.y);
+    // System.out.println(obj.z);
+    // System.out.prinln(obj.w);
+  } 
 }
   
 
