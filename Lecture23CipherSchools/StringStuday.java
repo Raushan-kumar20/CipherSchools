ce2bd09be71696312c5cdcdec52754a117f87783
Raushan-kumar20 <159602571+Raushan-kumar20@users.noreply.github.com>
2024-07-16 19:41:35 +0530

    Update StringStuday.java

--- Lecture23CipherSchools/StringStuday.java
+++ Lecture23CipherSchools/StringStuday.java
@@ -35,5 +35,70 @@ public class StringStudy{
     Systenm.out.println(partialStrFromArr);
 
     String partialStrFromArr2= new String(arr,2,3);
-    String.out.println(partialStrFromArr2)' 
+    String.out.println(partialStrFromArr2);
+
+    System.out.println("=========================================");
+    System.out.ptintlm("5. Changing the case");
+    System.out.println(name.toLowercase());
+    System.out.println(name.toUpperCase());
+    //name=name.toUpperCase();
+    System.out.println(name);//remain same
+
+    System.out.println("============================================");
+    System.out.println("6. Splitting");
+    System.out.println("Please enter your full name: ");
+    Scanner sc= new Scanner(System.in);
+    String fullName=sc.nextLine();
+    String strArr[] =fullName.split(" ");
+    for(int i=0;i<strArr.length;i++)
+      {
+        System.out.println(strArr[i]);
+      }
+    System.out.println("Please enter somthing separated by comes: ");
+    String csvText=sc.nextLine();
+    String csvArr[] =csvText.split(",");
+    for(int i=0;i<strArr.length;i++)
+      {
+        System.out.println(csvText[i]);
+      }
+    System.out.println("Please enter somthing separated by dots: ");
+    String dotText=sc.nextLine();
+    String dotArr[] =dotText.split(",");
+    for(int i=0;i<strArr.length;i++)
+      {
+        System.out.println(dotText[i]);
+      }
+    String backSlashText = "hello, I attend \"java\"class";
+    String backSlashArr[] = backSlashText.split("\");
+    for(int i=0;i<backSlashArr.length;i++)
+      {
+        System.out.println(backSlashArr[i]);
+      }
+    System.out.println("============================================");
+    System.out.println("7.Length of a String");
+    int len = name.length();
+    System.out.println("Length of: " +name+"is: "+len);
+     
+    System.out.println("============================================");
+    System.out.println("8.Finding index of a char in a String");
+    int index = name.indexOf('e');
+    System.out.println(Index of 'e' in : " +name+"is: "+index);
+      
+    int index2 = name.indexOf('E');
+    System.out.println(Index of 'E' in : " +name+"is: "+index2);
+
+    int index3 = name.indexOf('School');
+    System.out.println(Index of School in : " +name+"is: "+index3);
+
+    int index4 = name.indexOf('o',10);
+    System.out.println(Index of 'o' in : " +name+"is: "+index4);
+    int indexOfo=0;
+
+    //while(indexOfo != -1)
+    while(true)
+      {
+        indexOfo = name.indexOf('o',indexOfo +1);
+        if(indexOfo == -1) break;
+        System.out.printl("O found at: " +indexOfo);
+      }
   }
